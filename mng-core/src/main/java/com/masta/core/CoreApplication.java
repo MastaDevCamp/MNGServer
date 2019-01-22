41fb01f23a315bd32e146298f3363d901c975141
bearics <bearics@gmail.com>
2019-01-22 18:51:33 +0900

    Add swagger

---
+++ mng-core/src/main/java/com/masta/core/CoreApplication.java
@@ -0,0 +1,15 @@
+package com.masta.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class CoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoreApplication.class, args);
+    }
+
+}

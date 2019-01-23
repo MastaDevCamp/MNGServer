4f8904b0812ed5b89802efea38545ab3271ea95e
bearics <bearics@gmail.com>
2019-01-23 14:40:32 +0900

    Add getFileTreeList

--- mng-patch/src/main/java/com/masta/patch/PatchApplication.java
+++ mng-patch/src/main/java/com/masta/patch/PatchApplication.java
@@ -3,7 +3,9 @@ package com.masta.patch;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class PatchApplication {
 

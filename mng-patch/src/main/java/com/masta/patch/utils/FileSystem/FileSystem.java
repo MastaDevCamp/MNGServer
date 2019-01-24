6d143e5a22e0432a5521a42b9445c6bc4c9866f9
LAPTOP-5LF2RRRC\user <mor2222@naver.com>
2019-01-24 15:54:27 +0900

    processing ing

--- mng-patch/src/main/java/com/masta/patch/utils/FileSystem/FileSystem.java
+++ mng-patch/src/main/java/com/masta/patch/utils/FileSystem/FileSystem.java
@@ -1,8 +1,10 @@
 package com.masta.patch.utils.FileSystem;
 
-import com.masta.patch.utils.FileSystem.model.DiffFileEntry;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import com.masta.patch.utils.FileSystem.model.Version;
 import com.masta.patch.utils.FileSystem.model.FileEntry;
+import com.masta.patch.utils.FileSystem.model.Views;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -13,18 +15,22 @@ import java.util.List;
 @Service
 public class FileSystem {
 
-    private List<FileEntry> fileList;
+//    private List<FileEntry> fileList;
+    private List<ObjectWriter> fileList;
     int listIndex;
     int fileIndex;
 
-    public FileSystem(final List<FileEntry> fileList) {
+    private ObjectMapper mapper;
 
-        this.fileList = fileList;
+    public FileSystem(final List<FileEntry> fileList, final ObjectMapper mapper) {
+        this.mapper = mapper;
+//        this.fileList = fileList;
         this.listIndex = 1;
         this.fileIndex = 1;
     }
 
     public void listFilesForFolder(final File folder) {
+
         for (final File fileEntry : folder.listFiles()) {
             fileList.add(getFileEntry(fileEntry));
             if (fileEntry.isDirectory()) {
@@ -40,11 +46,20 @@ public class FileSystem {
      */
     public FileEntry getFileEntry(File file) {
 
+        boolean forPatch = false;
+
+        ObjectWriter viewWriter;
+        if (forPatch) {
+            viewWriter = mapper.writerWithView(Views.Patch.class);
+        } else {
+            viewWriter = mapper.writerWithView(Views.Full.class);
+        }
+
+
         // set file type
         char fileType = file.isDirectory() ? 'D' : (file.getTotalSpace() != 0 ? 'F' : 'G');
 
-
-        FileEntry fileEntry = DiffFileEntry.builder()
+        FileEntry fileEntry = FileEntry.builder()
                 .listIndex(listIndex++)
                 .type(fileType)
                 .path(file.getPath())
@@ -55,13 +70,18 @@ public class FileSystem {
                 .compressSize((int) file.length())
                 .originalHash("gieaorngoiarengionraeoigneariognoierango")
                 .compressHash("gig34ng90w43ng09qnero903oigrs9g0540p9roe")
+                .diffType('x') //현재는 full file
                 .build();
 
+
+        viewWriter.writeValue(viewWriter);
         return fileEntry;
     }
 
     public List<FileEntry> getFileTreeList(String path) {
         listFilesForFolder(new File(path));
+
+
         return this.fileList;
     }
 }
---
+++ mng-patch/src/main/java/com/masta/patch/utils/FileSystem/model/Views.java
@@ -0,0 +1,8 @@
+package com.masta.patch.utils.FileSystem.model;
+
+public class Views {
+
+    public static interface Full {}
+
+    public static interface Patch extends Full {}
+}

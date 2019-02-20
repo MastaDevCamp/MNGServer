8353d37c2ed8d5e99b5033d1c16b5ad0b4094c71
mjung1798 <mor2222@naver.com>
2019-02-20 20:28:38 +0900

    add annotation

--- mng-patch/src/main/java/com/masta/patch/api/AdminClientController.java
+++ mng-patch/src/main/java/com/masta/patch/api/AdminClientController.java
@@ -5,11 +5,9 @@ import com.masta.patch.model.JsonType;
 import com.masta.patch.service.AdminClientService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RequestMapping("admin")
 @RestController
 public class AdminClientController {
